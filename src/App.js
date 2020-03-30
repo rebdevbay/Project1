d0062ed5b5ce0cea47dc6aa3df891c93a6339681
barsovAsh <barsovashot@gmail.com>
2020-03-30 21:13:42 +0300

    ver.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Header } from "./Header";
 
 function App() {
+  let a = 2;
+  let b = 4;
+  let c = a + b;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hey world!</p>
+      <Header age={c} name="Pasha" />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// const summ = age => {
+//   let older = age + 22;
+//   return older;
+// };
+// export const Header = props => (
+//   <div className="HeaderDiv">
+//     Hello summ({props.name}) + {props.age}
+//   </div>
+// );
+
+export function Header(props) {
+  return (
+    <div className="HeaderDiv">
+      Hello summ({props.name}) + {props.age}
+    </div>
+  );
+}
