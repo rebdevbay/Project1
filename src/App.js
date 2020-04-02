c4e51b48c3bec39745c71f7e2e5bead0c2491781
barsovAsh <rebdevbay@gmail.com>
2020-04-02 21:10:38 +0300

    One HandleClick for two Buttons

--- src/App.js
+++ src/App.js
@@ -6,30 +6,26 @@ class App extends Component {
   state = {
     name: "unknown",
     lastName: "user",
-    age: 0
+    age: 0,
+    plus: false
   };
-  handleClick = (e) => {
-    this.setState(({ age }) => ({
-      age: ++age  
-    }));
+
+  handleClick = e => {
+    let id = e.target.id;
+    console.log(id);
+    this.setState(({ age }) => (id > 0 ? { age: ++age } : { age: --age }));
   };
-  handleminusClick = (e) => {
-    this.setState(({ age }) => ({
-      age: --age  
-    }));
-  };
+
   render() {
-   console.log (
-     typeof this.handleClick
-   )
-   
     const { name, lastName, age } = this.state;
     return (
       <div className="App-header">
-        <button onClick= {this.handleminusClick} />
-      <Header age={age} name={name} lastName={lastName} />
-        how old are you?
-        <button onClick= {this.handleClick} />
+        <Header
+          age={age}
+          name={name}
+          lastName={lastName}
+          handleClick={this.handleClick}
+        />
       </div>
     );
   }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,21 @@
 import React from "react";
 
 export function Header(props) {
-  const { name, lastName, age } = props;
+  const { name, lastName, age, handleClick } = props;
+  console.log(props);
   return (
     <div className="HeaderDiv">
       Task manager
       <div>
         Hi, {name} {lastName} - your old is {age}
+      </div>
+      <div className="HeaderDiv">
+        <button className="Header-link" id="1" onClick={handleClick}>
+          Pluss
+        </button>
+        <button className="Header-link" id="0" onClick={handleClick}>
+          Minus
+        </button>
       </div>
     </div>
   );
